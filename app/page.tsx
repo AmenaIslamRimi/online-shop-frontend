ef13deba0ba6f7edfa73d91b7d50dd636f8f408e
AmenaRimi5599 <amenarimi5@gmail.com>
2024-06-20 23:18:54 +0600

    new update

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ const Home = () => {
   return (
     <div>
       <h1>Home: 100 times ear pullings by isha</h1>
-      <div className="h-96 bg-slate-300 w-full my-auto flex items-center">
-        <p className="font-serif text-stone-950 border border-red-950 h-4/5 w-4/5 mx-auto">
+      <div className=" bg-slate-300 container m-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-5">
+        <p className="font-serif text-stone-950 border border-red-950  ">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus
           minima esse optio hic placeat qui dolor nostrum itaque voluptatem eum!
           Omnis autem quia dolor consequuntur eius. Optio, labore commodi.
@@ -17,21 +17,71 @@ const Home = () => {
           possimus sit! Lorem ipsum dolor sit amet, consectetur adipisicing
           elit. Debitis dicta nam quae cum, deleniti aspernatur tempore
           excepturi, sequi consequatur atque quidem. Quisquam delectus sint
-          perspiciatis illum modi saepe aut cumque? Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Aliquid velit fugiat reiciendis quaerat
-          commodi accusantium rerum at? Enim voluptates culpa soluta! Qui
-          provident ratione tempora, dignissimos molestias voluptate
-          perspiciatis sunt? Lorem ipsum dolor sit, amet consectetur adipisicing
-          elit. Suscipit, mollitia magnam aperiam ab eos quae recusandae
-          excepturi corporis minima minus, repellendus ex reprehenderit sapiente
-          cupiditate? Earum reiciendis deserunt modi dolor? Lorem ipsum dolor
-          sit, amet consectetur adipisicing elit. Debitis mollitia facilis
-          omnis, soluta itaque ad blanditiis culpa fugiat nesciunt officia sit
-          molestias alias tenetur totam expedita eius laborum voluptate
-          pariatur! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Consequuntur quas unde ducimus incidunt dicta atque necessitatibus
-          maiores qui illum ab in, molestiae vel illo repudiandae temporibus
-          perferendis laudantium quidem tenetur.
+          perspiciatis illum modi saepe aut cumque?
+        </p>
+        <p className="font-serif text-stone-950 border border-red-950 ">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus
+          minima esse optio hic placeat qui dolor nostrum itaque voluptatem eum!
+          Omnis autem quia dolor consequuntur eius. Optio, labore commodi.
+          Quidem. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Dolorem incidunt excepturi optio iure, hic accusamus esse quae
+          laudantium ea eaque, illum expedita! Molestias aliquid, quidem unde
+          placeat nisi pariatur accusamus! Lorem ipsum, dolor sit amet
+          consectetur adipisicing elit. Doloremque voluptas nihil nobis ut
+          exercitationem enim, tempora quidem saepe alias eveniet expedita
+          consectetur sapiente veniam atque consequatur odit accusantium
+          possimus sit! Lorem ipsum dolor sit amet, consectetur adipisicing
+          elit. Debitis dicta nam quae cum, deleniti aspernatur tempore
+          excepturi, sequi consequatur atque quidem. Quisquam delectus sint
+          perspiciatis illum modi saepe aut cumque?
+        </p>
+        <p className="font-serif text-stone-950 border border-red-950 ">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus
+          minima esse optio hic placeat qui dolor nostrum itaque voluptatem eum!
+          Omnis autem quia dolor consequuntur eius. Optio, labore commodi.
+          Quidem. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Dolorem incidunt excepturi optio iure, hic accusamus esse quae
+          laudantium ea eaque, illum expedita! Molestias aliquid, quidem unde
+          placeat nisi pariatur accusamus! Lorem ipsum, dolor sit amet
+          consectetur adipisicing elit. Doloremque voluptas nihil nobis ut
+          exercitationem enim, tempora quidem saepe alias eveniet expedita
+          consectetur sapiente veniam atque consequatur odit accusantium
+          possimus sit! Lorem ipsum dolor sit amet, consectetur adipisicing
+          elit. Debitis dicta nam quae cum, deleniti aspernatur tempore
+          excepturi, sequi consequatur atque quidem. Quisquam delectus sint
+          perspiciatis illum modi saepe aut cumque?
+        </p>
+        <p className="font-serif text-stone-950 border border-red-950 ">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus
+          minima esse optio hic placeat qui dolor nostrum itaque voluptatem eum!
+          Omnis autem quia dolor consequuntur eius. Optio, labore commodi.
+          Quidem. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Dolorem incidunt excepturi optio iure, hic accusamus esse quae
+          laudantium ea eaque, illum expedita! Molestias aliquid, quidem unde
+          placeat nisi pariatur accusamus! Lorem ipsum, dolor sit amet
+          consectetur adipisicing elit. Doloremque voluptas nihil nobis ut
+          exercitationem enim, tempora quidem saepe alias eveniet expedita
+          consectetur sapiente veniam atque consequatur odit accusantium
+          possimus sit! Lorem ipsum dolor sit amet, consectetur adipisicing
+          elit. Debitis dicta nam quae cum, deleniti aspernatur tempore
+          excepturi, sequi consequatur atque quidem. Quisquam delectus sint
+          perspiciatis illum modi saepe aut cumque?
+        </p>
+        <p className="font-serif text-stone-950 border border-red-950 col-span-2">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus
+          minima esse optio hic placeat qui dolor nostrum itaque voluptatem eum!
+          Omnis autem quia dolor consequuntur eius. Optio, labore commodi.
+          Quidem. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+          Dolorem incidunt excepturi optio iure, hic accusamus esse quae
+          laudantium ea eaque, illum expedita! Molestias aliquid, quidem unde
+          placeat nisi pariatur accusamus! Lorem ipsum, dolor sit amet
+          consectetur adipisicing elit. Doloremque voluptas nihil nobis ut
+          exercitationem enim, tempora quidem saepe alias eveniet expedita
+          consectetur sapiente veniam atque consequatur odit accusantium
+          possimus sit! Lorem ipsum dolor sit amet, consectetur adipisicing
+          elit. Debitis dicta nam quae cum, deleniti aspernatur tempore
+          excepturi, sequi consequatur atque quidem. Quisquam delectus sint
+          perspiciatis illum modi saepe aut cumque?
         </p>
       </div>
     </div>
