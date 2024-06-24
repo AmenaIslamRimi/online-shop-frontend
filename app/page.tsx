2feb28f66617298d49b6c08cd250dce110960c2a
AmenaRimi5599 <amenarimi5@gmail.com>
2024-06-24 22:02:47 +0600

    app-page, navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 const Home = () => {
   return (
     <div>
-      <h1>Home: 100 times ear pullings by isha</h1>
-      <div className=" bg-slate-300 container m-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-5">
+      <h1 className="text-slate-950">Home</h1>
+      {/* <div className=" bg-slate-300 container m-auto grid grid-cols-1  gap-5">
         <p className="font-serif text-stone-950 border border-red-950  ">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus
           minima esse optio hic placeat qui dolor nostrum itaque voluptatem eum!
@@ -83,6 +83,34 @@ const Home = () => {
           excepturi, sequi consequatur atque quidem. Quisquam delectus sint
           perspiciatis illum modi saepe aut cumque?
         </p>
+      </div> */}
+      <div className=" max-w-screen-xl  mx-auto my-3 grid grid-cols-3 gap-5 ">
+        <div className="bg-lime-200 rounded-full shadow-md h-96 w-full col-span-2 hover:bg-teal-200">
+          {/* <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae,
+            deleniti animi! Et libero maiores nemo unde omnis saepe laborum
+            minus autem distinctio ullam, inventore rerum pariatur voluptatibus,
+            illum veritatis quidem!
+          </p> */}
+        </div>
+        <div className="grid grid-rows-5 gap-5">
+          <div className="bg-lime-200 rounded-full shadow-md h-full w-full  hover:bg-teal-200"></div>
+          <div className="bg-lime-200 rounded-full shadow-md h-full w-full  hover:bg-teal-200"></div>
+          <div className="bg-lime-200 rounded-full shadow-md h-full w-full  hover:bg-teal-200"></div>
+          <div className="bg-lime-200 rounded-full shadow-md h-full w-full  hover:bg-teal-200"></div>
+          <div className="bg-lime-200 rounded-full shadow-md h-full w-full  hover:bg-teal-200"></div>
+        </div>
+      </div>
+      <div className="flex justify-center items-center">
+        <input
+          type="text"
+          className="border border-black rounded-md p-2 active:bg-red-200 focus:bg-blue-200 hover:bg-green-200 mx-5 my-5 "
+          placeholder="Enter your name"
+        />
+
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-5 my-5 ">
+          Submit
+        </button>
       </div>
     </div>
   );
